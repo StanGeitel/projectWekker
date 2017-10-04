66de09cb201c7ac6c77bb923b128cc8c073b85a4
StanGeitel <stan@geitel.nl>
2017-10-04 14:55:15 +0200

    alaram

--- projectBlock5/src/alarm.c
+++ projectBlock5/src/alarm.c
@@ -2,29 +2,36 @@
 #include "alarm.h"
 
 Problem problem;
+char alarmTime[5] = {'0','0',':','0','0'};
+unsigned char vol = 0;
 
 void alarmInit(void){
-	//I2C_Init();
-	//RTC_Init();
-	displayInit();
 	calculatorInit();
-	//init counter for seconds
-	//setMessage(getTime());
+	GPIO_Set(2, (0x07 << 2));
 }
 
-
-void solveProblem(void){
+void alarmOn(void){
 	Problem* pProblem = &problem;
 	setProblem(pProblem);
 	setMessage(pProblem->arr);
+	upVolume();
+
 	int userAnswer;
 	do {
 		do{
 			scanf("%d", &userAnswer); //get an answer via remote
 		}while(!userAnswer);
 		if (userAnswer != pProblem->answer) {
-			//volumeUp();
+			upVolume();
 		}
 	} while ( userAnswer != pProblem->answer);
 }
+
+void upVolume(void){
+	vol++;
+	GPIO_Set(2, (0x07 << 2));
+	GPIO_Clear(2, (0x1 << (vol + 2)));
+}
+
+
 
--- projectBlock5/src/alarm.h
+++ projectBlock5/src/alarm.h
@@ -8,14 +8,12 @@
 #ifndef ALARM_H_
 #define ALARM_H_
 
-#include <stdio.h>
 #include "LPC1769.h"
-#include "RealTimeClock.h"
 #include "display.h"
 #include "calculator.h"
 
-
 void alarmInit(void);
-void solveProblem(void);
+void alarmOn(void);
+void upVolume(void);
 
 #endif /* ALARM_H_ */
