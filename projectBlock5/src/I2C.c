4fbc3893c673f9516511d9cc0b3d1617f7c05ed0
ArjonArts220 <aja.arts@student.avans.nl>
2017-10-04 11:10:46 +0200

    I2C and RTC

--- projectBlock5/src/I2C.c
+++ projectBlock5/src/I2C.c
@@ -4,8 +4,12 @@
 
 void I2C_Init()
 {
-	PCONP |= 1 << 31;
-	PCLKSEL1 |= 1 << 6;
+	PCONP |= 1 << 7;
+	PCLKSEL0 |= 1 << 14;
+
+	PIN_SEL1 |= 1 << 22;
+	PIN_SEL1 |= 1 << 24;
+
 	I2C_SCLH |= 0x4B;
 	I2C_SCLL |= 0x4B;
 	I2C_CONSET = I2C_Conset_I2EN; //set i2c enable
